c9f114e80373b4aa90e0ddb1b3f0a06c4d70a6d2
Rits1701 <ritu.shikha17@gmail.com>
2026-06-19 12:15:32 +0200

    implement withdrawal use case

--- src/test/java/com/bank/AccountSpec.java
+++ src/test/java/com/bank/AccountSpec.java
@@ -4,14 +4,27 @@ import com.bank.application.BankAccountService;
 import com.bank.domain.model.Amount;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class AccountSpec {
+
+    private BankAccountService service;
 
     @Test
     void should_deposit_money() {
 
-        BankAccountService service =
-                new BankAccountService();
-
         service.deposit(Amount.of("1000"));
+    }
+
+    @Test
+    void should_withdraw_money() {
+        service.deposit(Amount.of("1000"));
+        service.withdraw(Amount.of("300"));
+
+        String statement = service.printStatement();
+
+        assertThat(statement)
+                .contains("-300")
+                .contains("700");
     }
 }
